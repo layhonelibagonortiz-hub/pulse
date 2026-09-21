ffa89218f97556d135e732170b034c0fb2029ff5
layhonelibagonortiz-hub <layhonelibagonortiz@gmail.com>
2026-09-21 01:25:02 +0000

    Use environment variables for MySQL configuration
    Updated database configuration to use environment variables.

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ app = Flask(__name__)
 app.secret_key = 'clave_secreta_pulse_2026'
 
 # --- CONFIGURACIÓN DE LA BASE DE DATOS ---
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pulse_db'
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+
 
 UPLOAD_FOLDER = os.path.join('static', 'videos')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
